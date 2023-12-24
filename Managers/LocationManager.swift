069c192c36e318638dc19312dce78363e389e76f
squattlife <migonyoung01@gmail.com>
2023-12-25 00:09:35 +0900

    Initial commit

---
+++ Managers/LocationManager.swift
@@ -0,0 +1,9 @@
+//
+//  LocationManager.swift
+//  QuickCast
+//
+//  Created by 이재영 on 2023/12/25.
+//
+
+import Foundation
+import CoreLocation
---
+++ QuickCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCastApp.swift
+//  QuickCast
+//
+//  Created by 이재영 on 2023/12/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
