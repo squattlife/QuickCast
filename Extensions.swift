ff121bf862c8b00719211907a1610aa9f3721b5b
squattlife <migonyoung01@gmail.com>
2023-12-26 02:57:17 +0900

    Add rounding doubles func

---
+++ Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  QuickCast
+//
+//  Created by 이재영 on 2023/12/26.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
